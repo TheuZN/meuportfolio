468d81b4a9050fa3781a98156899d024e0aa57cf
Mateus Melo <mateus.gon.melo@gmail.com>
2023-06-05 18:46:17 -0300

    atualizacoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Menu Mobile
 function menuShow() {
     let menuMobile = document.querySelector('.mobile_nav');
     if (menuMobile.classList.contains('open')) {
@@ -8,3 +9,18 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/img/close.svg";
     }
 }
+
+// Accordion Habilidades
+const accordionTitles = document.querySelectorAll(".title-accordion");
+
+  accordionTitles.forEach((accordionTitle) => {
+    accordionTitle.addEventListener("click", () => {
+     const height = accordionTitle.nextElementSibling.scrollHeight;
+     accordionTitle.classList.toggle("activite-accordion");
+     if (accordionTitle.classList.contains("activite-accordion"))  {
+       accordionTitle.nextElementSibling.style.maxHeight = `${height}px`; 
+    } else {
+        accordionTitle.nextElementSibling.style.maxHeight = "0px"; 
+    }
+  });
+});
