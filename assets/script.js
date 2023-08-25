f97eddbab85d3a9611304ba3392fb4a384f06738
Mateus Melo <mateus.gon.melo@gmail.com>
2023-08-24 21:45:46 -0300

    novos arquivos

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,66 @@
+// Form Verify
+
+const formfield = document.querySelectorAll("[required]");
+
+formfield.forEach((field) => {
+  field.addEventListener("blur", () => fieldVerify(field));
+  field.addEventListener("invalid", event => event.preventDefault());
+});
+
+const errorList = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'tooShort',
+];
+
+const messageList = {
+  name: {
+      valueMissing: "Por favor, preencha este campo.",
+      patternMismatch: "Por favor, preencha com um nome válido.",
+      tooShort: "Por favor, preencha com um nome válido."
+  },
+  email: {
+      valueMissing: "Por favor, preencha este campo.",
+      typeMismatch: "Por favor, preencha um email válido.",
+      tooShort: "Por favor, preencha um e-mail válido."
+  },
+  subject: {
+    valueMissing: "Por favor, preencha este campo.",
+  },
+  message: {
+    valueMissing: "Por favor, preencha este campo.",
+  },
+};
+
+function fieldVerify(field) {
+  let message = "";
+  field.setCustomValidity("");
+
+  errorList.forEach(error => {
+      if (field.validity[error]) {
+          message = messageList[field.name][error];
+      }
+  });
+
+  const messageError = field.parentNode.querySelector(".message");
+  const validityInput = field.checkValidity();
+
+  if (!validityInput) {
+      messageError.textContent = message;
+  } else {
+      messageError.textContent = "";
+  }
+}
+
+
+
 // Menu Mobile
-function menuShow() {
+
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+
+openMenu.addEventListener("click", function (){
   let menuMobile = document.querySelector('.mobile-nav');
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open');
@@ -8,15 +69,15 @@ function menuShow() {
       menuMobile.classList.add('open');
       document.querySelector('.ico').src = "assets/img/close.svg";
   }
-}
+})
 
-function closeMenu() {
+closeMenu.addEventListener("click", function (){
   let menuMobile = document.querySelector('.mobile-nav');
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open');
       document.querySelector('.ico').src = "assets/img/open.svg";
   }
-}
+})
 
 // Accordion Skills
 const accordionTitles = document.querySelectorAll(".title-accordion");
